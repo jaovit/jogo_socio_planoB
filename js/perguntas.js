f072f3a345fcef20b14c7a2b61ac17ba3705df8b
jão <110256135+jaovit@users.noreply.github.com>
2023-08-08 14:27:28 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,321 @@
+var quest_m = [
+    {
+        ptexto: "O que são indicadores sociais?",
+        opcoes: [
+            "Medidas que quantificam aspectos da vida das pessoas e da sociedade",
+            "Indicativos de tendências de mercado",
+            "Métricas financeiras utilizadas em empresas",
+            "Dados demográficos de um país"
+        ],
+        resposta: "Medidas que quantificam aspectos da vida das pessoas e da sociedade",
+        dica: "Indicadores sociais são medidas numéricas que ajudam a entender e avaliar diferentes aspectos da sociedade, como saúde, educação, renda, entre outros."
+    },
+    {
+        ptexto: "Qual é a importância dos indicadores sociais?",
+        opcoes: [
+            "Eles ajudam a tomar decisões financeiras",
+            "Eles não têm impacto significativo na tomada de decisões",
+            "Eles são apenas dados estatísticos sem utilidade real",
+            "Eles auxiliam na formulação e avaliação de políticas públicas"
+        ],
+        resposta: "Eles auxiliam na formulação e avaliação de políticas públicas",
+        dica: "Os indicadores sociais são ferramentas essenciais para orientar o planejamento e a avaliação de políticas públicas, visando melhorar a qualidade de vida da população."
+    },
+    {
+        ptexto: "Qual é o objetivo dos indicadores de saúde?",
+        opcoes: [
+            "Acompanhar o desempenho financeiro das empresas de saúde",
+            "Comparar o tamanho da população em diferentes regiões",
+            "Avaliar a qualidade de vida da população por meio de aspectos relacionados à saúde",
+            "Definir a expectativa de vida de um país"
+        ],
+        resposta: "Avaliar a qualidade de vida da população por meio de aspectos relacionados à saúde",
+        dica: "Os indicadores de saúde quantificam informações sobre a saúde da população, como taxas de mortalidade, expectativa de vida, incidência de doenças, entre outros."
+    },
+    {
+        ptexto: "O que é o Índice de Desenvolvimento Humano (IDH)?",
+        opcoes: [
+            "Um indicador que mede apenas a renda média da população",
+            "Um indicador que avalia a qualidade de vida com base em três dimensões: saúde, educação e renda",
+            "Um indicador exclusivamente econômico que mede o PIB de um país",
+            "Um indicador que analisa apenas a taxa de alfabetização de um país"
+        ],
+        resposta: "Um indicador que avalia a qualidade de vida com base em três dimensões: saúde, educação e renda",
+        dica: "O Índice de Desenvolvimento Humano (IDH) é um indicador que busca avaliar o desenvolvimento de um país considerando sua qualidade de vida, abrangendo saúde, educação e renda."
+    },
+    {
+        ptexto: "O que é a taxa de mortalidade infantil?",
+        opcoes: [
+            "A taxa de óbitos de idosos em um país",
+            "A taxa de natalidade em relação à população total",
+            "A taxa de óbitos de crianças menores de um ano a cada mil nascidos vivos",
+            "A taxa de crescimento populacional de crianças"
+        ],
+        resposta: "A taxa de óbitos de crianças menores de um ano a cada mil nascidos vivos",
+        dica: "A taxa de mortalidade infantil é um indicador que mede a quantidade de crianças que morrem antes de completar um ano de vida a cada mil nascidos vivos."
+    },
+
+    {
+        ptexto: "O que é analfabetismo funcional?",
+        opcoes: [
+            "Incapacidade de ler e escrever",
+            "Incapacidade de compreender e utilizar informações escritas no dia a dia",
+            "Incapacidade de fazer cálculos matemáticos simples",
+            "Incapacidade de falar em público"
+        ],
+        resposta: "Incapacidade de compreender e utilizar informações escritas no dia a dia",
+        dica: "O analfabetismo funcional se refere à dificuldade que uma pessoa tem em compreender e usar informações escritas em sua vida cotidiana, mesmo que seja capaz de ler e escrever."
+    },
+    {
+        ptexto: "O que é o coeficiente de Gini?",
+        opcoes: [
+            "Um índice que mede a taxa de inflação em um país",
+            "Um indicador que avalia a desigualdade de renda em uma sociedade",
+            "Uma métrica que quantifica o crescimento econômico",
+            "Uma medida que analisa a taxa de desemprego"
+        ],
+        resposta: "Um indicador que avalia a desigualdade de renda em uma sociedade",
+        dica: "O coeficiente de Gini é utilizado para medir o grau de desigualdade de renda em uma sociedade, variando de 0 (igualdade perfeita) a 1 (desigualdade total)."
+    },
+    {
+        ptexto: "O que é o índice de pobreza?",
+        opcoes: [
+            "Um indicador que mede a quantidade de pessoas em situação de extrema riqueza",
+            "Um índice que avalia o custo de vida em diferentes cidades",
+            "Um indicador que quantifica a quantidade de pessoas em situação de pobreza em uma sociedade",
+            "Uma métrica que analisa a quantidade de empréstimos bancários"
+        ],
+        resposta: "Um indicador que quantifica a quantidade de pessoas em situação de pobreza em uma sociedade",
+        dica: "O índice de pobreza é uma medida que avalia a porcentagem de pessoas que vivem abaixo da linha de pobreza em uma sociedade."
+    },
+    {
+        ptexto: "O que é o índice de desenvolvimento social?",
+        opcoes: [
+            "Um indicador que mede apenas o crescimento econômico de um país",
+            "Um índice que avalia a qualidade de vida considerando saúde e educação",
+            "Um indicador exclusivo da área de saúde",
+            "Um índice que analisa a quantidade de impostos em um país"
+        ],
+        resposta: "Um índice que avalia a qualidade de vida considerando saúde e educação",
+        dica: "O índice de desenvolvimento social é uma medida que considera diversos fatores, como saúde, educação e padrões de vida, para avaliar a qualidade de vida em uma sociedade."
+    },
+    {
+        ptexto: "O que é o índice de Gini inverso?",
+        opcoes: [
+            "Um índice que mede a igualdade de renda de forma invertida",
+            "Uma métrica que avalia a desigualdade de gênero",
+            "Um indicador que considera a taxa de desemprego de forma invertida",
+            "Uma medida que analisa o acesso à educação de forma inversa"
+        ],
+        resposta: "Um índice que mede a igualdade de renda de forma invertida",
+        dica: "O índice de Gini inverso é uma forma de medir a igualdade de renda, mas de maneira invertida, onde um valor maior indica mais igualdade e um valor menor indica mais desigualdade."
+    },
+
+    {
+        ptexto: "O que é o Índice de Desenvolvimento Humano (IDH)?",
+        opcoes: [
+            "Um indicador que mede apenas a expectativa de vida da população",
+            "Um índice que avalia a renda per capita de um país",
+            "Um indicador que considera a taxa de natalidade de uma sociedade",
+            "Um índice composto que avalia a expectativa de vida, educação e renda"
+        ],
+        resposta: "Um índice composto que avalia a expectativa de vida, educação e renda",
+        dica: "O Índice de Desenvolvimento Humano (IDH) é uma medida composta que avalia a qualidade de vida de uma população com base na expectativa de vida, educação e renda per capita."
+    },
+    {
+        ptexto: "O que é a taxa de fecundidade?",
+        opcoes: [
+            "A quantidade de nascimentos em um país em determinado período de tempo",
+            "A taxa de mortalidade infantil em uma região",
+            "A quantidade de casamentos em um país em determinado período de tempo",
+            "A proporção de idosos em relação à população total"
+        ],
+        resposta: "A quantidade de nascimentos em um país em determinado período de tempo",
+        dica: "A taxa de fecundidade se refere à quantidade média de filhos nascidos vivos por mulher em idade fértil (geralmente entre 15 e 49 anos) em um determinado período de tempo."
+    },
+    {
+        ptexto: "O que é o Índice de Desenvolvimento Urbano (IDU)?",
+        opcoes: [
+            "Um índice que mede apenas o desenvolvimento econômico de áreas urbanas",
+            "Um indicador que avalia a qualidade de vida em áreas rurais",
+            "Um índice que considera a taxa de natalidade de áreas urbanas",
+            "Uma medida composta que avalia a qualidade de vida em áreas urbanas"
+        ],
+        resposta: "Uma medida composta que avalia a qualidade de vida em áreas urbanas",
+        dica: "O Índice de Desenvolvimento Urbano (IDU) é uma medida composta que avalia diversos aspectos da qualidade de vida em áreas urbanas, incluindo saúde, educação e renda."
+    },
+    {
+        ptexto: "O que é o Índice de Gini ajustado por equivalência?",
+        opcoes: [
+            "Uma versão simplificada do coeficiente de Gini",
+            "Um indicador que considera a inflação ao calcular a desigualdade de renda",
+            "Um índice que leva em conta a composição familiar ao medir a desigualdade",
+            "Uma métrica que analisa a desigualdade de gênero"
+        ],
+        resposta: "Um índice que leva em conta a composição familiar ao medir a desigualdade",
+        dica: "O Índice de Gini ajustado por equivalência é uma versão do coeficiente de Gini que leva em consideração a composição familiar das pessoas ao medir a desigualdade de renda."
+    },
+    {
+        ptexto: "O que é o Índice de Desenvolvimento da Educação Básica (IDEB)?",
+        opcoes: [
+            "Um índice que mede o nível de escolaridade de uma população",
+            "Um indicador que avalia apenas a qualidade das escolas particulares",
+            "Um índice que considera apenas a infraestrutura das escolas",
+            "Um indicador que avalia a qualidade do ensino nas escolas públicas"
+        ],
+        resposta: "Um indicador que avalia a qualidade do ensino nas escolas públicas",
+        dica: "O Índice de Desenvolvimento da Educação Básica (IDEB) é um indicador que avalia a qualidade do ensino nas escolas públicas do Brasil, levando em consideração o desempenho dos alunos em avaliações padronizadas."
+    },
+
+    {
+        ptexto: "O que é o Índice de Desenvolvimento da Primeira Infância (IDPI)?",
+        opcoes: [
+            "Um índice que mede a expectativa de vida das crianças",
+            "Um indicador que avalia o desenvolvimento cognitivo das crianças",
+            "Um índice que considera a qualidade das creches e pré-escolas",
+            "Um indicador que avalia o desenvolvimento integral das crianças até 6 anos"
+        ],
+        resposta: "Um indicador que avalia o desenvolvimento integral das crianças até 6 anos",
+        dica: "O Índice de Desenvolvimento da Primeira Infância (IDPI) é um indicador que avalia o desenvolvimento integral das crianças até 6 anos de idade, levando em consideração diversos aspectos como saúde, educação, proteção e convivência familiar."
+    },
+    {
+        ptexto: "O que é o Índice de Vulnerabilidade das Famílias (IVF)?",
+        opcoes: [
+            "Um índice que mede a renda média das famílias",
+            "Um indicador que avalia o grau de risco social das famílias",
+            "Um índice que considera apenas a composição familiar",
+            "Um indicador que avalia a escolaridade das famílias"
+        ],
+        resposta: "Um indicador que avalia o grau de risco social das famílias",
+        dica: "O Índice de Vulnerabilidade das Famílias (IVF) é um indicador que avalia o grau de risco social das famílias com base em critérios como renda, educação, moradia e acesso a serviços públicos."
+    },
+    {
+        ptexto: "O que é o Coeficiente de Mortalidade Infantil?",
+        opcoes: [
+            "Um índice que mede a expectativa de vida das crianças",
+            "Um indicador que avalia a taxa de natalidade",
+            "Um índice que considera a proporção de idosos",
+            "Um indicador que avalia o número de óbitos de crianças menores de 1 ano a cada mil nascidos vivos"
+        ],
+        resposta: "Um indicador que avalia o número de óbitos de crianças menores de 1 ano a cada mil nascidos vivos",
+        dica: "O Coeficiente de Mortalidade Infantil é um indicador que avalia a quantidade de óbitos de crianças menores de 1 ano a cada mil nascidos vivos, sendo um indicativo importante da qualidade do sistema de saúde e das condições de vida da população."
+    },
+    {
+        ptexto: "O que é o Índice de Alfabetização?",
+        opcoes: [
+            "Um índice que mede a renda média das pessoas",
+            "Um indicador que avalia o grau de escolaridade",
+            "Um índice que considera apenas a capacidade de leitura",
+            "Um indicador que avalia a capacidade de leitura e escrita das pessoas"
+        ],
+        resposta: "Um indicador que avalia a capacidade de leitura e escrita das pessoas",
+        dica: "O Índice de Alfabetização é um indicador que avalia a capacidade de leitura e escrita das pessoas em determinada faixa etária, sendo um importante indicativo do nível educacional de uma população."
+    },
+    {
+        ptexto: "O que é o Índice de Pobreza Multidimensional (IPM)?",
+        opcoes: [
+            "Um índice que mede apenas a renda das famílias",
+            "Um indicador que avalia apenas o nível de escolaridade",
+            "Um índice que considera diversas dimensões da pobreza, como saúde, educação e padrão de vida",
+            "Um indicador que avalia a desigualdade social"
+        ],
+        resposta: "Um índice que considera diversas dimensões da pobreza, como saúde, educação e padrão de vida",
+        dica: "O Índice de Pobreza Multidimensional (IPM) é um indicador que avalia a pobreza levando em consideração múltiplas dimensões, como saúde, educação e padrão de vida, fornecendo uma visão mais abrangente da situação de pobreza em."
+
+    }];
+
+
+
+var quest_l = [
+    {
+        ptexto: 'O que são indicadores sociais?',
+        opcoes: ['Um novo tipo de emoji para redes sociais',
+            'Uma unidade de medida para selfies',
+            'Ponteiros em relógios que apontam para direções sociais',
+            'Medidas que avaliam aspectos econômicos da sociedade'],
+        resposta: 'Medidas que avaliam aspectos econômicos da sociedade',
+        dica: 'Eles medem diferentes aspectos da sociedade'
+    },
+    {
+        ptexto: 'Qual é a finalidade dos indicadores sociais?',
+        opcoes: ['Comparar a popularidade de gatos e cachorros nas redes sociais',
+            'Promover competições de dança entre indicadores',
+            'Criar gráficos coloridos para enfeitar relatórios',
+            'Avaliar e mensurar aspectos da qualidade de vida e bem-estar da população'],
+        resposta: 'Avaliar e mensurar aspectos da qualidade de vida e bem-estar da população',
+        dica: 'Eles ajudam a entender como as pessoas vivem e estão bem'
+    },
+    {
+        ptexto: 'O que significa PIB?',
+        opcoes: ['Pipoca, Iogurte e Brigadeiro',
+            'Produto Interno Bruto',
+            'Pão, Iogurte e Biscoito',
+            'Projeto de Indicadores Brilhantes'],
+        resposta: 'Produto Interno Bruto',
+        dica: 'É uma medida econômica importante'
+    },
+    {
+        ptexto: 'O que é Índice de Gini?',
+        opcoes: ['Um modelo de guitarra muito popular',
+            'Uma medida de desigualdade de renda',
+            'Um indicador da qualidade de ginástica praticada',
+            'Um aparelho que mede o índice de gengibre em alimentos'],
+        resposta: 'Uma medida de desigualdade de renda',
+        dica: 'Refere-se à distribuição de renda em uma sociedade'
+    },
+
+    {
+        ptexto: 'O que é taxa de alfabetização?',
+        opcoes: ['Uma taxa que mede quantos livros alguém lê por ano',
+            'A porcentagem de pessoas que sabem soletrar a palavra "alfabetização"',
+            'Um indicador de quantos lápis são usados para escrever',
+            'A porcentagem de pessoas com habilidades de leitura e escrita'],
+        resposta: 'A porcentagem de pessoas com habilidades de leitura e escrita',
+        dica: 'Refere-se à capacidade das pessoas lerem e escreverem'
+    },
+    {
+        ptexto: 'O que é expectativa de vida?',
+        opcoes: ['O tempo que alguém espera para chegar ao fim de um filme',
+            'Quantos anos alguém espera para ganhar na loteria',
+            'A média de anos que as pessoas esperam viver',
+            'O tempo que alguém espera para receber o pedido de comida'],
+        resposta: 'A média de anos que as pessoas esperam viver',
+        dica: 'Indica quanto tempo as pessoas geralmente vivem'
+    },
+    {
+        ptexto: 'O que é taxa de desemprego?',
+        opcoes: ['A taxa de pessoas que estão cansadas de trabalhar',
+            'A porcentagem de pessoas que preferem não ter um emprego',
+            'A média de empregos que uma pessoa tem durante a vida',
+            'A porcentagem de pessoas sem emprego em relação à força de trabalho'],
+        resposta: 'A porcentagem de pessoas sem emprego em relação à força de trabalho',
+        dica: 'Indica quantas pessoas estão sem emprego em relação ao total de pessoas aptas a trabalhar'
+    },
+    {
+        ptexto: 'O que é taxa de fertilidade?',
+        opcoes: ['A porcentagem de plantas que dão frutos',
+            'O número de ovos que uma galinha põe por dia',
+            'A média de bebês que nascem em uma família',
+            'A porcentagem de casais que preferem não ter filhos'],
+        resposta: 'A média de bebês que nascem em uma família',
+        dica: 'Indica quantos bebês, em média, nascem em famílias'
+    },
+    {
+        ptexto: 'O que é índice de Gini?',
+        opcoes: ['Um índice que mede o nível de seriedade de uma pessoa chamada Gini',
+            'Um ranking das pessoas chamadas Gini por ordem alfabética',
+            'Um indicador de desigualdade de renda em uma sociedade',
+            'Um índice que mede o nível de felicidade das pessoas chamadas Gini'],
+        resposta: 'Um indicador de desigualdade de renda em uma sociedade',
+        dica: 'Avalia o quão igualitária ou desigual é a distribuição de renda em uma sociedade'
+    },
+    {
+        ptexto: 'O que é taxa de natalidade?',
+        opcoes: ['A porcentagem de bebês que nascem no Natal',
+            'A média de presentes de Natal por família',
+            'A porcentagem de pessoas que preferem não celebrar o Natal',
+            'A taxa de nascimentos por mil habitantes em um ano'],
+        resposta: 'A taxa de nascimentos por mil habitantes em um ano',
+        dica: 'Indica quantos bebês nascem a cada mil habitantes em um ano'
+    }
+];
